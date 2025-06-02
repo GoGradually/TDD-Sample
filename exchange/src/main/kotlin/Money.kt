b680cc6ddde408405325fd2304f0a5511c2a2207
GoGradually <chkun3109@gmail.com>
2025-06-02 17:45:33 +0900

    feat: 10장 - 흥미로운 시간: 공용 times 메소드 도입

--- exchange/src/main/kotlin/Money.kt
+++ exchange/src/main/kotlin/Money.kt
@@ -1,11 +1,38 @@
-abstract class Money (protected open val amount: Int){
-    companion object{
-        fun dollar(amount: Int): Dollar{
+open class Money(protected open val amount: Int, open val currency: String) {
+    companion object {
+        fun dollar(amount: Int): Dollar {
             return Dollar(amount, "USD")
         }
-        fun franc(amount: Int): Franc{
+
+        fun franc(amount: Int): Franc {
             return Franc(amount, "CHF")
         }
     }
-    abstract fun times(multiplier: Int): Money
+
+    open fun times(multiplier: Int): Money {
+        return Money(amount * multiplier, currency)
+    }
+
+    fun currency(): String {
+        return currency
+    }
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        other as Money
+
+        return amount == other.amount && currency() == other.currency
+    }
+
+    override fun hashCode(): Int {
+        var result = amount
+        result = 31 * result + currency.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "Money(amount=$amount, currency='$currency')"
+    }
+
 }
